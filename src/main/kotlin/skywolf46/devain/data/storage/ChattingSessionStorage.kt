e434d0983e4da523684cfcb9f84dafebc96b86f0
skywolf46 <skywolf461@gmail.com>
2023-05-02 01:53:14 +0900

    feat: 채팅 세션 잠금 추가

--- src/main/kotlin/skywolf46/devain/data/storage/ChattingSessionStorage.kt
+++ src/main/kotlin/skywolf46/devain/data/storage/ChattingSessionStorage.kt
@@ -1,6 +1,8 @@
 package skywolf46.devain.data.storage
 
-import skywolf46.devain.data.sessions.ChattingSession
+import arrow.core.Option
+import arrow.core.toOption
+import skywolf46.devain.data.parsed.gpt.sessions.ChattingSession
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
@@ -8,15 +10,19 @@ class ChattingSessionStorage {
     private val sessions = mutableMapOf<Pair<Long, Long>, ChattingSession>()
     private val lock = ReentrantLock()
 
+    fun acquireSession(guildId: Long, userId: Long): Option<ChattingSession> {
+        return sessions[guildId to userId].toOption()
+    }
+
     fun removeSession(guildId: Long, userId: Long): Boolean {
         return lock.withLock {
             sessions.remove(guildId to userId) != null
         }
     }
 
-    fun resetSession(guildId: Long, userId: Long) {
+    fun resetSession(guildId: Long, userId: Long, model: String) {
         lock.withLock {
-            sessions.put(guildId to userId, ChattingSession(userId))
+            sessions.put(guildId to userId, ChattingSession(userId, model))
         }
     }
 }
