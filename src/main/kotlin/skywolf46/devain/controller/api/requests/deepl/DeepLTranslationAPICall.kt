37ba4c2f773110c0f05d0e62628c4ab7f4abe040
skywolf46 <skywolf461@gmail.com>
2023-09-16 07:49:12 +0900

    feat(api-call): DeepL Translation Call에 데이터 기록 추가

--- src/main/kotlin/skywolf46/devain/controller/api/requests/deepl/DeepLTranslationAPICall.kt
+++ src/main/kotlin/skywolf46/devain/controller/api/requests/deepl/DeepLTranslationAPICall.kt
@@ -7,12 +7,16 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import org.json.simple.parser.JSONParser
 import org.koin.core.component.get
+import skywolf46.devain.KEY_DEEPL_PROCEED_COUNT
+import skywolf46.devain.KEY_DEEPL_PROCEED_TOKEN
 import skywolf46.devain.controller.api.APICall
 import skywolf46.devain.controller.api.APIError
 import skywolf46.devain.controller.api.error.PreconditionError
 import skywolf46.devain.controller.api.error.UnexpectedError
+import skywolf46.devain.controller.api.requests.devain.DevAinUpdatePersistenceCountAPICall
 import skywolf46.devain.model.rest.deepl.translation.DeepLTranslateRequest
 import skywolf46.devain.model.rest.deepl.translation.DeepLTranslateResponse
+import skywolf46.devain.model.rest.devain.data.request.UpdateRequest
 import skywolf46.devain.util.getList
 import skywolf46.devain.util.getMap
 import skywolf46.devain.util.parseMap
@@ -26,6 +30,8 @@ class DeepLTranslationAPICall(private val apiKey: String, client: Option<HttpCli
         if (apiKey.endsWith(":fx")) DEEPL_FREE_TRANSLATION_ENDPOINT else DEEPL_PRO_TRANSLATION_ENDPOINT
     private val client = client.getOrElse { get() }
     private val parser = get<JSONParser>()
+    private val updateCall = get<DevAinUpdatePersistenceCountAPICall>()
+
 
     override suspend fun call(request: DeepLTranslateRequest): Either<APIError, DeepLTranslateResponse> {
         return runCatching {
@@ -40,7 +46,11 @@ class DeepLTranslationAPICall(private val apiKey: String, client: Option<HttpCli
             val resultMap = parser.parseMap(result.bodyAsText())
             if ("translations" !in resultMap)
                 return UnexpectedError(IllegalStateException("Translation not found in response (${resultMap})")).left()
-            DeepLTranslateResponse.fromJson(resultMap.getList("translations").getMap(0)).right()
+            DeepLTranslateResponse.fromJson(resultMap.getList("translations").getMap(0))
+                .apply {
+                    updateCall.call(UpdateRequest(KEY_DEEPL_PROCEED_COUNT, 1L))
+                    updateCall.call(UpdateRequest(KEY_DEEPL_PROCEED_TOKEN, translationResult.length.toLong()))
+                }.right()
         }.getOrElse {
             UnexpectedError(it).left()
         }
