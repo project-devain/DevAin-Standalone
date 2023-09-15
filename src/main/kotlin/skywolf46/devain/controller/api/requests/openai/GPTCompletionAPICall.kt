72d326bdf65c29917b9af7b975c3399399b6b5a8
skywolf46 <skywolf461@gmail.com>
2023-09-16 07:48:50 +0900

    feat(api-call): GPT Call에 데이터 기록 추가

--- src/main/kotlin/skywolf46/devain/controller/api/requests/openai/GPTCompletionAPICall.kt
+++ src/main/kotlin/skywolf46/devain/controller/api/requests/openai/GPTCompletionAPICall.kt
@@ -7,11 +7,16 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import org.json.simple.parser.JSONParser
 import org.koin.core.component.get
+import skywolf46.devain.KEY_GPT_PROCEED_COUNT
+import skywolf46.devain.KEY_GPT_PROCEED_TIME
+import skywolf46.devain.KEY_GPT_PROCEED_TOKEN
 import skywolf46.devain.controller.api.APICall
 import skywolf46.devain.controller.api.APIError
 import skywolf46.devain.controller.api.error.PreconditionError
 import skywolf46.devain.controller.api.error.StandardRestAPIError
 import skywolf46.devain.controller.api.error.UnexpectedError
+import skywolf46.devain.controller.api.requests.devain.DevAinUpdatePersistenceCountAPICall
+import skywolf46.devain.model.rest.devain.data.request.UpdateRequest
 import skywolf46.devain.model.rest.gpt.completion.request.OpenAIGPTRequest
 import skywolf46.devain.model.rest.gpt.completion.response.OpenAIGPTResponse
 import skywolf46.devain.util.parseMap
@@ -22,6 +27,8 @@ class GPTCompletionAPICall(private val apiKey: String, client: Option<HttpClient
     APICall<OpenAIGPTRequest, OpenAIGPTResponse> {
     private val client = client.getOrElse { get() }
     private val parser = get<JSONParser>()
+    private val updateCall = get<DevAinUpdatePersistenceCountAPICall>()
+
     override suspend fun call(request: OpenAIGPTRequest): Either<APIError, OpenAIGPTResponse> {
         return runCatching {
             val prebuiltRequest = request.asJson().getOrElse { return PreconditionError(it).left() }
@@ -36,7 +43,11 @@ class GPTCompletionAPICall(private val apiKey: String, client: Option<HttpClient
             if (result.status.value != 200) {
                 return StandardRestAPIError(result.status.value, result.bodyAsText()).left()
             }
-            OpenAIGPTResponse.fromJson(parser.parseMap(result.bodyAsText())).right()
+            OpenAIGPTResponse.fromJson(parser.parseMap(result.bodyAsText())).apply {
+                updateCall.call(UpdateRequest(KEY_GPT_PROCEED_COUNT, 1L))
+                updateCall.call(UpdateRequest(KEY_GPT_PROCEED_TOKEN, usage.completionToken.toLong()))
+                updateCall.call(UpdateRequest(KEY_GPT_PROCEED_TIME, (System.currentTimeMillis() - request.createdOn)))
+            }.right()
         }.getOrElse {
             UnexpectedError(it).left()
         }
