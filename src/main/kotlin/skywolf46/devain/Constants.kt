16e35f871b04c661c08c492a0d43427caa0ebe1a
skywolf46 <skywolf461@gmail.com>
2023-09-16 07:46:04 +0900

    feat: 상수 보관용 클래스 Constants 추가

---
+++ src/main/kotlin/skywolf46/devain/Constants.kt
@@ -0,0 +1,12 @@
+package skywolf46.devain
+
+const val SQLITE_PERSISTENCE = "devain_persistence"
+const val TABLE_PERSISTENCE_COUNT = "counter"
+
+const val KEY_GPT_PROCEED_COUNT = "proceed_count"
+const val KEY_GPT_PROCEED_TOKEN = "proceed_token"
+const val KEY_GPT_PROCEED_TIME = "proceed_time"
+
+
+const val KEY_DEEPL_PROCEED_COUNT = "deepl_proceed_count"
+const val KEY_DEEPL_PROCEED_TOKEN = "deepl_proceed_token"
