6a8bc09e304e417f9ed44db86803c218a0ad4776
skywolf46 <skywolf461@gmail.com>
2023-09-15 09:47:04 +0900

    chore: 주석 추가

--- src/main/kotlin/skywolf46/devain/model/rest/gpt/completion/request/OpenAIGPTRequest.kt
+++ src/main/kotlin/skywolf46/devain/model/rest/gpt/completion/request/OpenAIGPTRequest.kt
@@ -8,17 +8,80 @@ import skywolf46.devain.model.rest.gpt.completion.OpenAIGPTMessage
 import skywolf46.devain.util.checkRangeAndFatal
 
 data class OpenAIGPTRequest(
+    /**
+     * OpenAI Chat Completion model name.
+     */
     val modelName: String,
+    /**
+     * Messages to send.
+     * If last message is not USER, error will be thrown.
+     */
     val messages: List<OpenAIGPTMessage>,
+    /**
+     * Amount of message to generate.
+     */
     val generateAmount: Int = 1,
+    /**
+     * Temperature of text generation.
+     * Default value is 1.0.
+     *
+     * If temperature is high, text will be more random.
+     * If temperature is low, text will be more predictable.
+     *
+     * Range: 0.0 ~ 2.0
+     */
     val temperature: Option<Double> = None,
+    /**
+     * top_p value of text generation.
+     * Default value is 1.0.
+     *
+     * If top_p is high, text will be more random.
+     * If top_p is low, text will be more predictable.
+     *
+     * Range: 0.0 ~ 1.0
+     */
     val top_p: Option<Double> = None,
+    /**
+     * Max token count of text generation.
+     * Default value is infinite(None).
+     *
+     * Range: 1 ~ Infinite
+     */
     val maxTokens: Option<Int> = None,
+    /**
+     * Presence penalty of text generation.
+     * Default value is 0.0.
+     *
+     * Positive values penalize new tokens based on whether they appear in the text so far,
+     *  increasing the model's likelihood to talk about new topics.
+     *
+     * Range: -2.0 ~ 2.0
+     */
     val presencePenalty: Option<Double> = None,
+    /**
+     * Frequency penalty of text generation.
+     * Default value is 0.0.
+     *
+     * Positive values penalize new tokens based on their existing frequency in the text so far,
+     *  decreasing the model's likelihood to repeat the same line verbatim.
+     *
+     * Range: -2.0 ~ 2.0
+     */
     val frequencyPenalty: Option<Double> = None,
+    /**
+     * Whether to hide prompt or not.
+     * Default value is false.
+     */
     val hidePrompt: Boolean = false,
+    /**
+     * Timestamp of request.
+     */
     val createdOn: Long = System.currentTimeMillis()
 ) : Request<JSONObject> {
+
+    /**
+     * Struct JSONObject from this request.
+     */
     override fun asJson(): Either<Throwable, JSONObject> {
         val map = JSONObject()
         temperature.tap {
@@ -47,6 +110,12 @@ data class OpenAIGPTRequest(
         }
 
         map["model"] = modelName
+        if (messages.isEmpty()) {
+            return IllegalArgumentException("최소 1개 이상의 메시지 내역을 포함해야 합니다.").left()
+        }
+        if (messages.last().role != OpenAIGPTMessage.Role.USER) {
+            return IllegalArgumentException("마지막 메시지는 USER 역할이어야 합니다.").left()
+        }
         map["messages"] = JSONArray().apply {
             addAll(messages.map { it.asJson().getOrNull()!! })
         }
