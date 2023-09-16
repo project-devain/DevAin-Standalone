b8b41c36f96df8d8c414429835f7db52679f7fb3
skywolf46 <skywolf461@gmail.com>
2023-09-16 23:52:50 +0900

    feat(plugin): 기능 분류를 위해 플러그인 기능 추가

---
+++ src/main/kotlin/skywolf46/devain/platform/plugin/PluginModule.kt
@@ -0,0 +1,38 @@
+package skywolf46.devain.platform.plugin
+
+import org.koin.core.component.KoinComponent
+import java.io.File
+
+abstract class PluginModule(val pluginName: String) : KoinComponent {
+
+    lateinit var dataDirectory: File
+        internal set
+
+    open fun canBeLoaded(): Boolean {
+        return true
+    }
+
+    open fun onPreInitialize() {
+        // Do nothing
+    }
+
+    open fun onInitialize() {
+        // Do nothing
+    }
+
+    open fun onPostInitialize() {
+        // Do nothing
+    }
+
+    open suspend fun getStatistics(): Map<String, List<PluginStatistics>> {
+        return emptyMap()
+    }
+
+    open fun getVersion(): String {
+        return "unspecified"
+    }
+
+    data class PluginStatistics(val name: String, val value: String)
+
+
+}
