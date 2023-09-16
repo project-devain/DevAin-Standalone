d8576cc76cc866be716ff620cd56097bf46b28ee
skywolf46 <skywolf461@gmail.com>
2023-09-16 23:49:32 +0900

    feat: DALLE / Dream Studio 상수 추가

--- src/main/kotlin/skywolf46/devain/Constants.kt
+++ src/main/kotlin/skywolf46/devain/Constants.kt
@@ -7,6 +7,13 @@ const val KEY_GPT_PROCEED_COUNT = "proceed_count"
 const val KEY_GPT_PROCEED_TOKEN = "proceed_token"
 const val KEY_GPT_PROCEED_TIME = "proceed_time"
 
+const val KEY_DALLE_PROCEED_COUNT = "dalle_proceed_count"
+const val KEY_DALLE_PROCEED_TIME = "dalle_proceed_time"
+
+
+const val KEY_DREAM_STUDIO_SD_PROCEED_COUNT = "dreamstudio_sd_proceed_count"
+const val KEY_DREAM_STUDIO_SD_PROCEED_TIME = "dreamstudio_sddalle_proceed_time"
+
 
 const val KEY_DEEPL_PROCEED_COUNT = "deepl_proceed_count"
 const val KEY_DEEPL_PROCEED_TOKEN = "deepl_proceed_token"
