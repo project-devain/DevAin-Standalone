6a004d593e4d607dc515c97b4299c331cb517347
skywolf46 <skywolf461@gmail.com>
2024-04-27 03:03:58 +0900

    feat: 누락된 Command R+ 클래스 커밋

---
+++ src/main/kotlin/skywolf46/devain/model/api/rplus/RPlusResponse.kt
@@ -0,0 +1,14 @@
+package skywolf46.devain.model.api.rplus
+
+import org.json.simple.JSONObject
+import skywolf46.devain.apicall.networking.Response
+
+class RPlusResponse(val text: String) : Response {
+    companion object {
+        fun fromJson(jsonObject: JSONObject) : RPlusResponse {
+            return RPlusResponse(
+                jsonObject["text"].toString()
+            )
+        }
+    }
+}
