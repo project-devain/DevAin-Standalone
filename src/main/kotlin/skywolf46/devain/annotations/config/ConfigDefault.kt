7bfb7f54c002d73822a34cef798d46ca5fb41346
skywolf46 <skywolf461@gmail.com>
2024-04-27 03:02:21 +0900

    feat: 콘피그 형태 개별 콘피그로 변경

---
+++ src/main/kotlin/skywolf46/devain/annotations/config/ConfigDefault.kt
@@ -0,0 +1,7 @@
+package skywolf46.devain.annotations.config
+
+class ConfigDefault {
+    annotation class Int(val default: kotlin.Int)
+
+    annotation class String(val default: kotlin.String)
+}
---
+++ src/main/kotlin/skywolf46/devain/model/data/config/ConfigElement.kt
@@ -0,0 +1,3 @@
+package skywolf46.devain.model.data.config
+
+interface ConfigElement
