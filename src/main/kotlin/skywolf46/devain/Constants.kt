9b1e7d3bd6d7022bba34374a58c4d6c2afd3de50
skywolf46 <skywolf461@gmail.com>
2024-04-27 03:09:57 +0900

    feat: 상수 변수 추가

--- src/main/kotlin/skywolf46/devain/Constants.kt
+++ src/main/kotlin/skywolf46/devain/Constants.kt
@@ -14,11 +14,14 @@ const val KEY_DALLE_PROCEED_COUNT = "dalle_proceed_count"
 const val KEY_DALLE_PROCEED_TIME = "dalle_proceed_time"
 
 const val KEY_COHERE_GENERATION_PROCEED_COUNT = "cohere_proceed_count"
+const val KEY_COMMAND_R_PLUS_GENERATION_PROCEED_COUNT = "command_r_plus_proceed_count"
 
+const val KEY_GROQ_GENERATION_PROCEED_COUNT = "groq_plus_proceed_count"
 
 const val KEY_DREAM_STUDIO_SD_PROCEED_COUNT = "dreamstudio_sd_proceed_count"
 const val KEY_DREAM_STUDIO_SD_PROCEED_TIME = "dreamstudio_sddalle_proceed_time"
 
-
 const val KEY_DEEPL_PROCEED_COUNT = "deepl_proceed_count"
 const val KEY_DEEPL_PROCEED_TOKEN = "deepl_proceed_token"
+
+const val K_CURRENCY_API_CALL = "currency_api_call"
