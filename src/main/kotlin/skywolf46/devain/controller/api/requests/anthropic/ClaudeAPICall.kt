430e432e666214a2155be69b5acdde3258d05565
skywolf46 <skywolf461@gmail.com>
2024-04-27 03:03:18 +0900

    feat: Claude 지원 추가

---
+++ src/main/kotlin/skywolf46/devain/controller/api/requests/anthropic/ClaudeAPICall.kt
@@ -0,0 +1,30 @@
+package skywolf46.devain.controller.api.requests.anthropic
+
+import arrow.core.Either
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.right
+import io.ktor.client.*
+import io.ktor.http.*
+import org.json.simple.JSONObject
+import skywolf46.devain.apicall.APIError
+import skywolf46.devain.apicall.RESTAPICall
+import skywolf46.devain.model.api.anthropic.ClaudeGenerationRequest
+import skywolf46.devain.model.api.anthropic.ClaudeGenerationResponse
+
+class ClaudeAPICall(private val apiKey: String, client: Option<HttpClient> = None) :
+    RESTAPICall<ClaudeGenerationRequest, ClaudeGenerationResponse>(
+        { req -> "https://api.anthropic.com/v1/messages" }, client, HttpMethod.Post
+    ) {
+    override suspend fun parseResult(
+        request: ClaudeGenerationRequest,
+        response: JSONObject
+    ): Either<APIError, ClaudeGenerationResponse> {
+        return ClaudeGenerationResponse.fromJson(response).right()
+    }
+
+    override fun HeadersBuilder.applyCredential() {
+        append("x-api-key", apiKey)
+        append("anthropic-version", "2023-06-01")
+    }
+}
---
+++ src/main/kotlin/skywolf46/devain/model/api/anthropic/ClaudeGenerationRequest.kt
@@ -0,0 +1,22 @@
+package skywolf46.devain.model.api.anthropic
+
+import arrow.core.Either
+import arrow.core.Option
+import arrow.core.right
+import org.json.simple.JSONObject
+import skywolf46.devain.apicall.networking.Request
+
+class ClaudeGenerationRequest(
+    val model: String,
+    val messages: List<ClaudeMessage>,
+    val maxTokens: Option<Int>,
+) : Request<JSONObject> {
+    override fun serialize(): Either<Throwable, JSONObject> {
+        val obj = JSONObject()
+        obj["model"] = model
+        obj["messages"] = messages.map { it.serialize().orNull() }
+        maxTokens.map { obj["max_tokens"] = it }
+        return obj.right()
+    }
+
+}
